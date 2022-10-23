448155c009190c3957a5bf3c6d2b1453bfbb59a9
florian <florian.devin13@gmail.com>
2022-10-23 23:45:21 +0200

    v0.5 rapport

--- src/TOTEX_vs_GWP.py
+++ src/TOTEX_vs_GWP.py
@@ -1,22 +1,43 @@
 import output
 import matplotlib.pyplot as plt
+import os
+ 
+
+def getFoldersName(path) :
+    return os.listdir(path)
 
 
-FolderName_l = ["Run0", "Run1", "Run2"]
+FolderName_l = getFoldersName("I:\\Telechargements\\20_10\\")
 
 Data_l  = []
 TOTEX_l = []
 GWP_l   = []
 
 for fileName in FolderName_l :
-    Data_l.append(output.outData("./data/"+fileName))
+    Data_l.append(output.outData("I:\\Telechargements\\20_10\\"+fileName))
 
 
 for run in Data_l :
     TOTEX_l.append(run.TOTEX.getValue())
     GWP_l.append(run.GWP_total.getValue())
 
-plt.plot(TOTEX_l, GWP_l)
+plt_with_nuc = plt.plot(TOTEX_l, GWP_l, "y*")
+FolderName_l = getFoldersName("I:\\Telechargements\\20_10_without_nuclear\\")
+
+Data_l  = []
+TOTEX_l = []
+GWP_l   = []
+
+for fileName in FolderName_l :
+    Data_l.append(output.outData("I:\\Telechargements\\20_10_without_nuclear\\"+fileName))
+
+print(GWP_l)
+
+for run in Data_l :
+    TOTEX_l.append(run.TOTEX.getValue())
+    GWP_l.append(run.GWP_total.getValue())
+plt_without_nuc = plt.plot(TOTEX_l, GWP_l, "b*")
+plt.xlabel("TOTEX")
+plt.ylabel("GWP")
+plt.legend(['With nuclear', 'Without nuclear'])
 plt.show()
-
-
